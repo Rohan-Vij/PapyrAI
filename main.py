cbed6c1f53d4df906d104e98b609e726d16c1991
Rohan-V <rohankvij@gmail.com>
2023-06-18 19:42:08 -0700

    Finished user auth

--- main.py
+++ main.py
@@ -24,80 +24,68 @@ db = client["papyrai"]
 users = db["users"]
 
 # user management
-@app.route("/", methods=['post', 'get'])
-def index():
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
     if "email" in session:
         return redirect(url_for("logged_in"))
 
     if request.method == "POST":
-        name = request.form.get("fullname")
-        email = request.form.get("email")
-        password = request.form.get("password1")
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
 
-        # if found in database showcase that it's found
-        email_found = users.find_one({"email": email})
-
-        if email_found:
-            return render_template('index.html')
+        if users.find_one({"email": email}):
+            return render_template('index.html', message="An account with that email address already exists.")
 
         else:
-            # hash the password and encode it
             hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            # assing them in a dictionary in key value pairs
-            user_input = {'name': name, 'email': email, 'password': hashed}
-            # insert it in the record collection
-            users.insert_one(user_input)
-
-            # find the new created account and its email
-            user_data = users.find_one({"email": email})
-            new_email = user_data['email']
-
-            # if registered redirect to logged in as the registered user
-            return render_template('logged_in.html')
+            users.insert_one({'name': name, 'email': email, 'password': hashed})
+            session['email'] = email
+            
+            return redirect(url_for("home"))
     return render_template('index.html')
 
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
     if "email" in session:
         return redirect(url_for("logged_in"))
 
     if request.method == "POST":
-        email = request.form.get("email")
-        password = request.form.get("password")
+        email = request.form['email']
+        password = request.form['pass']
 
-        # check if email exists in database
-        email_found = users.find_one({"email": email})
-        if email_found:
-            email_val = email_found['email']
-            passwordcheck = email_found['password']
-
-            # encode the password and check if it matches
-            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
-                session["email"] = email_val
-                return redirect(url_for('logged_in'))
+        # Make sure that account does exist
+        email_query = users.find_one({"email": email})
+        if email_query:
+            if bcrypt.checkpw(password.encode('utf-8'), email_query['password']):
+                session['email'] = email
+                return redirect(url_for("home"))
             else:
-                if "email" in session:
-                    return redirect(url_for("logged_in"))
-                return render_template('login.html')
+                return render_template("login.html", message="Incorrect password.")
         else:
-            return render_template('login.html')
-    return render_template('login.html')
+            return render_template("login.html", message="An account with that email address does not exist.")
+    return render_template("login.html")
 
 
-@app.route('/logged_in')
-def logged_in():
+@app.route('/home')
+def home():
     if "email" in session:
-        email = session["email"]
-        return render_template('logged_in.html', email=email)
+        return render_template('home.html')
     else:
         return redirect(url_for("login"))
 
 
-@app.route("/logout", methods=["POST", "GET"])
+@app.route("/logout")
 def logout():
     if "email" in session:
         session.pop("email", None)
         return render_template("signout.html")
     else:
         return render_template('index.html')
+
+@app.before_request
+def before_request():
+    g.user = None
+    if "user" in session:
+        g.user = session['user']
