dd541ba4b16e93976d3002cd3570282b9303ae62
Ayush Garg <ayushgargemail@gmail.com>
2023-06-21 18:15:05 -0700

    changed mail.py to use data

--- mail.py
+++ mail.py
@@ -76,7 +76,7 @@ def data_to_html(data):
       """
 
 
-def send_email(receiver_email):
+def send_email(receiver_email, data):
     # loading env variables
     load_dotenv()
 
@@ -91,61 +91,8 @@ def send_email(receiver_email):
     message["To"] = receiver_email
 
     # Create the plain-text and HTML version of your message
-    text = """\
-    Discover Custom Research Papers with Papyr AI
-    ==============================================
-
-    Stay informed about the latest research in your field!
-
-    Example Research Paper 1
-    ------------------------
-    Explore groundbreaking findings in your research domain through this example research paper.
-
-    Authors: John Doe, Jane Smith
-    Published: June 2023
-    Keywords: Artificial Intelligence, Machine Learning, Data Analysis
-
-    Example Research Paper 2
-    ------------------------
-    Dive into cutting-edge findings with this hand-picked example research paper tailored to your interests.
-
-    Authors: Sarah Johnson, Michael Brown
-    Published: June 2023
-    Keywords: Data Science, Natural Language Processing, Deep Learning
-    """
-
-    html = """\
-    <html>
-    <body style="margin: 0; padding: 0; font-family: 'Roboto', Arial, sans-serif;">
-    <div style="width: 100%; max-width: 600px; margin: 0 auto; background-color: #fafafa; padding: 20px; font-family: 'Roboto', Arial, sans-serif;">
-      <div style="text-align: center; margin-bottom: 20px;">
-        <h1 style="font-size: 24px; color: #3f51b5;">Discover Research Papers with Papyr AI</h1>
-        <p style="font-size: 16px; color: #616161;">Stay informed about the latest research in your field!</p>
-      </div>
-
-      <div style="display: block; margin-bottom: 30px;">
-        <div style="background-color: #ffffff; padding: 20px; margin-bottom: 20px; border-radius: 4px; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
-          <img src="article1-thumbnail.jpg" alt="Article 1 Thumbnail" style="width: 100%; max-width: 300px; height: auto; margin-bottom: 10px;">
-          <h2 style="font-size: 20px; font-weight: bold; margin-bottom: 10px; color: #3f51b5;">Example Research Paper 1</h2>
-          <p style="font-size: 16px; line-height: 1.5; color: #616161;">Explore groundbreaking findings in your research domain through this example research paper.</p>
-          <p style="font-size: 14px; margin-top: 10px; color: #757575;"><strong>Authors:</strong> John Doe, Jane Smith</p>
-          <p style="font-size: 14px; margin-top: 5px; color: #757575;"><strong>Published:</strong> June 2023</p>
-          <p style="font-size: 14px; margin-top: 5px; color: #757575;"><strong>Keywords:</strong> Artificial Intelligence, Machine Learning, Data Analysis</p>
-        </div>
-
-        <div style="background-color: #ffffff; padding: 20px; border-radius: 4px; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
-          <img src="article2-thumbnail.jpg" alt="Article 2 Thumbnail" style="width: 100%; max-width: 300px; height: auto; margin-bottom: 10px;">
-          <h2 style="font-size: 20px; font-weight: bold; margin-bottom: 10px; color: #3f51b5;">Example Research Paper 2</h2>
-          <p style="font-size: 16px; line-height: 1.5; color: #616161;">Dive into cutting-edge findings with this hand-picked example research paper tailored to your interests.</p>
-          <p style="font-size: 14px; margin-top: 10px; color: #757575;"><strong>Authors:</strong> Sarah Johnson, Michael Brown</p>
-          <p style="font-size: 14px; margin-top: 5px; color: #757575;"><strong>Published:</strong> June 2023</p>
-          <p style="font-size: 14px; margin-top: 5px; color: #757575;"><strong>Keywords:</strong> Data Science, Natural Language Processing, Deep Learning</p>
-        </div>
-      </div>
-    </div>
-    </body>
-    </html>
-    """
+    text = data_to_plaintext(data)
+    html = data_to_html(data)
 
     # Turn these into plain/html MIMEText objects
     part1 = MIMEText(text, "plain")
@@ -163,16 +110,18 @@ def send_email(receiver_email):
         server.sendmail(sender_email, receiver_email, message.as_string())
 
 
-# # Call the function to send the email
-# receiver_email = "rohankvij@gmail.com"  # Replace with the desired receiver email
-# send_email(receiver_email)
-
-from api import standardize_arXiv
-import requests, xmltodict, json
-
+# Call the function to send the email
+import json
+receiver_email = "rohankvij@gmail.com"  # Replace with the desired receiver email
 
 with open("tmp/arXiv_schema_clean.json", "r") as fin:
     data = json.load(fin)
     data["published"] = datetime.now()
+    send_email(receiver_email, [data])
 
-print(data_to_html([data]))
+
+# with open("tmp/arXiv_schema_clean.json", "r") as fin:
+#     data = json.load(fin)
+#     data["published"] = datetime.now()
+
+# print(data_to_html([data]))
